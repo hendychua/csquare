b302194f8bee782c7d17dbf5d64865662e6ef3e2
hendychua <hendychua@gmail.com>
2013-06-01 17:39:59 +0800

    commit before merge

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 from flask import Flask
 from flask import render_template
+import sqlite3
+
+PARSE_APP_ID = "Ag4XfW4UyetRRHaphILYG6b95cBCOxjdGWOEwjXz"
+PARSE_REST_API_KEY = "BI1PVdG16Es2G4t0w2HQGsTnAkNrhRmFJaXgIGLv"
+
+DEBUG_MODE = True
+PORT = 5000
+HOST = '127.0.0.1'
 
 app = Flask(__name__)
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
 
 @app.route("/")
-def hello():
-    return render_template("index.html", test="hello world")
+def index():
+    return "index"
+    
+@app.route("/admin")
+def admin():
+    return "admin dashboard"
+    
+@app.route("/participant")
+def participant():
+    return "participant dashboard"
+    
+@app.route("/classes")
+def admin_classes():
+    return "admin classes"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host=HOST, port=PORT, debug=DEBUG_MODE)
