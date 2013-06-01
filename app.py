0c60923f78360e9eda5986cb6b279f3b02b3bb80
lcplcy <lcp_lcy@hotmail.com>
2013-06-01 23:55:56 +0800

    updated teacher

--- app.py
+++ app.py
@@ -11,5 +11,13 @@ def index():
 def teacher_db():
     return render_template("teacher_dashboard.html")
 
+@app.route("/student")
+def student_db():
+    return render_template("student_dashboard.html")
+
+@app.route("/class")
+def class_pg():
+    return render_template("class_page.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
