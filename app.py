f8cd17424196634e137ef67c7e4538c135307a42
lcplcy <lcp_lcy@hotmail.com>
2013-06-01 17:33:57 +0800

    teacher_dashboard and index

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template("index.html", test="hello world")
+def index():
+    return render_template("index.html")
+
+@app.route("/teacher")
+def teacher_db():
+    return render_template("teacher_dashboard.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
